0cf0588ec92944f42fe997d5de46cc1279690e5d
elyassir <yassir.elmassafi@gmail.com>
2023-09-25 01:08:20 +0100

    add

--- new/srcs/requirements/wordpress/tools/script.sh
+++ new/srcs/requirements/wordpress/tools/script.sh
@@ -14,11 +14,28 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	mv wp-cli.phar /usr/local/bin/wp;
 	cd /var/www/html;
 	wp core download --allow-root;
-	mv /var/www/wp-config.php /var/www/html/
+	mv /wp-config.php /var/www/html/
 	echo "Wordpress: creating users..."
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
 	echo "Wordpress: set up!"
 fi
 
+# hna wa9ila khasni ndir sed l dak file dyal wp-config.php bach nbadal les parametres dyal database
+
+# if you want the installtion page not to be displayed, you can add the following line to the wp-config.php file
+
+#https://localhost/wp-login.php
+
+# Database name
+# Database username
+# Database password
+# Database host
+
+# define('DB_NAME', 'database_name_here');
+# define('DB_USER', 'username_here');
+# define('DB_PASSWORD', 'password_here');
+# define('DB_HOST', 'localhost');
+
 exec "$@"
+# exec "$@" = execute the command line arguments passed to the script
