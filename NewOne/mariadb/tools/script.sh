1192b55d5141b350f57af7f303343765b071c4ab
elyassir <yassir.elmassafi@gmail.com>
2023-10-31 06:08:25 -0400

    add

--- NewOne/mariadb/tools/script.sh
+++ NewOne/mariadb/tools/script.sh
@@ -2,12 +2,22 @@
 
 service mysql start
 
+MYSQL_ROOT_PASSWORD=123456
+MYSQL_USER=wp_user
+MYSQL_PASSWORD=wp_password
+DB_NAME=wp_db
+ROOT=root
+
 mysql << EOF
-CREATE DATABASE IF NOT EXISTS wpdb;
-CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY 'dbpassword';
-GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'%';
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%';
+ALTER USER '$ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
+
 sed -i "s/#port/port/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i "s/bind-address/#bind-address/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# ALTER USER is used to modify the properties of a MariaDB user account.
--- NewOne/wordpress/tools/script.sh
+++ NewOne/wordpress/tools/script.sh
@@ -9,27 +9,26 @@ mkdir -p /var/
 mkdir -p /var/www/
 mkdir -p /var/www/html/
 
-cd /var/www/html/
-
 # -d checks if the path is a directory
 
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
-chmod +x wp-cli.phar;
-mv wp-cli.phar /bin/wp;
 
 if [ ! -d "wordpress" ]; then
+
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+    chmod +x wp-cli.phar;
+    mv wp-cli.phar /bin/wp;
 
     mkdir wordpress
     cd wordpress
     wp core download --allow-root;
     cp wp-config-sample.php wp-config.php;
-	sed -i "s/database_name_here/wpdb/g" wp-config.php;
-	sed -i "s/username_here/wpuser/g" wp-config.php;
-	sed -i "s/password_here/dbpassword/g" wp-config.php;
+	sed -i "s/database_name_here/$DB_NAME/g" wp-config.php;
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php;
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php;
     sed -i "s/localhost/mariadb/g" wp-config.php;
-    
-wp core install --allow-root --url=yel-mass.42.fr --title=ft_server --admin_user=admin --admin_password=admin --admin_email=you@example.com
-wp user create johndoe johndoe@example.com --role=editor --first_name=John --last_name=Doe --user_pass=johndoe --allow-root
+
+    wp core install --allow-root --url=$DOMAIN_NAME --title=$SERVER_NAME --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+    wp user create $WP_USER $WP_USER_EMAIL --role=editor --first_name=$WP_USER_FIRST_NAME --last_name=$WP_USER_LAST_NAME --user_pass=$WP_USER_PASSWORD --allow-root
 
 else
 
@@ -37,7 +36,8 @@ else
 
 fi
 
+mkdir -p /run/php/
 
-service php7.3-fpm start
+# while true; do sleep 1000; done
 
-while true; do sleep 1000; done
+exec /usr/sbin/php-fpm7.3 -F
