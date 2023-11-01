9be5aa03506577f7d133a1cdb36644d57080a449
elyassir <yassir.elmassafi@gmail.com>
2023-10-31 22:46:30 -0400

    add

--- NewOne/mariadb/tools/script.sh
+++ NewOne/mariadb/tools/script.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-service mysql start
+service mysql start # !
 
-MYSQL_ROOT_PASSWORD=123456
-MYSQL_USER=wp_user
-MYSQL_PASSWORD=wp_password
-DB_NAME=wp_db
-ROOT=root
+sleep 3
 
 mysql << EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%';
-ALTER USER '$ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE IF NOT EXISTS wp_db;
+CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY 'wp_password';
+GRANT ALL PRIVILEGES ON wp_db.* TO 'wp_user'@'%';
 FLUSH PRIVILEGES;
 EOF
-
 
 sed -i "s/#port/port/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i "s/bind-address/#bind-address/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
+service mysql stop
+
 # ALTER USER is used to modify the properties of a MariaDB user account.
+
+mysqld
